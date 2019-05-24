116debe022019366d0dbc3deb0ce3764fc132a45
AejandroGH97 <alejandrogoicochea@gmail.com>
2019-05-24 17:58:22 -0500

    Funcionalidad con std::array

--- disjoint_set.h
+++ disjoint_set.h
@@ -1,23 +1,23 @@
 //
 // Created by alejandro on 5/24/19.
 //
-
+#include <array>
 #ifndef UNION_FIND_ALEJANDROGH97_DISJOINT_SET_H
 #define UNION_FIND_ALEJANDROGH97_DISJOINT_SET_H
 
-template <template<typename ,typename...>class Container>
+template <int N,class Container>
 
 class disjoint_set{
 
 private:
 
-    Container<int> vertices;
-    Container<int> root;
+    Container vertices;
+    Container root;
 
 public:
-    explicit disjoint_set(int n):vertices(n*n),root(n*n) {
+    disjoint_set():vertices(N*N),root(N*N) {
 
-        for (int i = 0; i < n*n; ++i) {
+        for (int i = 0; i < N*N; ++i) {
             vertices[i]=i;
             root[i]=i;
         }
@@ -28,6 +28,10 @@ public:
             temp=root[temp];
         }
         return temp;
+    }
+
+    int findCompresion(int num){
+
     }
 
     bool same(int vert1, int vert2) {
@@ -49,6 +53,57 @@ public:
         root[find(vert1)]=vert2;
     }
 };
+
+
+template <int N>
+class disjoint_set<N,std::array<int,N>>{
+
+private:
+
+    std::array<int,N*N> vertices;
+    std::array<int,N*N> root;
+
+public:
+     disjoint_set(){
+
+        for (int i = 0; i < N*N; ++i) {
+            vertices[i]=i;
+            root[i]=i;
+        }
+    }
+    int find(int num) {
+        int temp = num;
+        while(temp!=root[temp]){
+            temp=root[temp];
+        }
+        return temp;
+    }
+
+    int findCompresion(int num){
+
+    }
+
+    bool same(int vert1, int vert2) {
+        int temp1 = vert1;
+        int temp2 = vert2;
+
+        while(temp1!=root[temp1]){
+            temp1=root[temp1];
+        }
+
+        while(temp2!=root[temp2]){
+            temp2=root[temp2];
+        }
+
+        return temp1==temp2;
+    }
+
+    void join(int vert1, int vert2) {
+        root[find(vert1)]=vert2;
+    }
+};
+
+
 
 
 #endif //UNION_FIND_ALEJANDROGH97_DISJOINT_SET_H
--- disjoint_setTest.cpp
+++ disjoint_setTest.cpp
@@ -9,7 +9,7 @@
 
 TEST_CASE("Test arena:") {
 
-    disjoint_set<std::vector> test1(5);
+    disjoint_set<5,std::vector<int>> test1;
 
     test1.join(1,0);
     test1.join(3,1);
@@ -26,6 +26,42 @@ TEST_CASE("Test arena:") {
     REQUIRE(test1.same(8,5)==true);
     REQUIRE(test1.same(3,5)==false);
     REQUIRE(test1.same(2,7)==true);
+
+    disjoint_set<5,std::deque<int>> test2;
+
+    test2.join(1,0);
+    test2.join(3,1);
+    test2.join(2,0);
+    test2.join(7,0);
+    test2.join(8,4);
+    test2.join(4,5);
+    test2.join(6,4);
+    REQUIRE(test2.find(1)==0);
+    REQUIRE(test2.find(0)==0);
+    REQUIRE(test2.find(3)==0);
+    REQUIRE(test2.find(8)==test2.find(6));
+    REQUIRE(test2.find(7)==test2.find(3));
+    REQUIRE(test2.same(8,5)==true);
+    REQUIRE(test2.same(3,5)==false);
+    REQUIRE(test2.same(2,7)==true);
+
+    disjoint_set<5,std::array<int,5>> test3;
+
+    test3.join(1,0);
+    test3.join(3,1);
+    test3.join(2,0);
+    test3.join(7,0);
+    test3.join(8,4);
+    test3.join(4,5);
+    test3.join(6,4);
+    REQUIRE(test3.find(1)==0);
+    REQUIRE(test3.find(0)==0);
+    REQUIRE(test3.find(3)==0);
+    REQUIRE(test3.find(8)==test2.find(6));
+    REQUIRE(test3.find(7)==test2.find(3));
+    REQUIRE(test3.same(8,5)==true);
+    REQUIRE(test3.same(3,5)==false);
+    REQUIRE(test3.same(2,7)==true);
 
 
 }
